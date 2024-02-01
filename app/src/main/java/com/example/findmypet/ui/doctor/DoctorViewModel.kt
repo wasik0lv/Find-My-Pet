2a85791195528ab1ed65794d2ce561eac0eb521c
wasik0lv <113295720+wasik0lv@users.noreply.github.com>
2024-02-01 22:35:39 +0700

    1st

---
+++ app/src/main/java/com/example/findmypet/ui/doctor/DoctorViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.findmypet.ui.doctor
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DoctorViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Doctor Fragment"
+    }
+    val text: LiveData<String> = _text
+}
