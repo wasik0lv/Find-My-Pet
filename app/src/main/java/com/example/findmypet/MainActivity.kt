a05bc6a2c3c209e29c22e49812034ce72bf35bcb
wasik0lv <113295720+wasik0lv@users.noreply.github.com>
2024-02-13 01:46:51 +0700

    toolbar and bottom nav
    toolbar and bottom nav almost final version

--- app/src/main/java/com/example/findmypet/MainActivity.kt
+++ app/src/main/java/com/example/findmypet/MainActivity.kt
@@ -1,35 +1,47 @@
 package com.example.findmypet
 
+
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.findmypet.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
+
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+        val toolbar: Toolbar = binding.toolbar
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_progression, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_progression, R.id.navigation_petcard,
+                R.id.navigation_doctor , R.id.navigation_account
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        toolbar.setupWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
     }
 }
--- app/src/main/java/com/example/findmypet/ui/petcard/PetcardViewModel.kt
+++ app/src/main/java/com/example/findmypet/ui/petcard/PetcardViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.findmypet.ui.doctor
+package com.example.findmypet.ui.petcard
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DoctorViewModel : ViewModel() {
+class PetcardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is doctor Fragment"
+        value = "This is petcard Fragment"
     }
     val text: LiveData<String> = _text
 }
